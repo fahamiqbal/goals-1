0eb7b53171fceab59391bb5892ee5ba193c2c7c7
Faham Iqbal <l174140@lhr.nu.edu.pk>
2023-06-17 13:07:13 -0700

    Adding styles

--- src/app/layouts/modules/Accounts/accounts-detail-view/accounts-detail-view.component.ts
+++ src/app/layouts/modules/Accounts/accounts-detail-view/accounts-detail-view.component.ts
@@ -1,5 +1,7 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,AfterContentInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/core/crm-api/api.service';
 // Initialization for ES Users
 import {
   Tab,
@@ -12,17 +14,38 @@ import {
   templateUrl: './accounts-detail-view.component.html',
   styleUrls: ['./accounts-detail-view.component.scss']
 })
-export class AccountsDetailViewComponent implements OnInit {
+export class AccountsDetailViewComponent implements OnInit, AfterContentInit {
   name: string = '';
   isEditable: boolean = false;
 
   activeTab: string = 'tab1';
   myForm: FormGroup;
+  id: string;
 
-
-  constructor(private formBuilder: FormBuilder)
+  constructor(private formBuilder: FormBuilder,private apiService: ApiService,private route: ActivatedRoute)
   {
       initTE({ Tab });
+  }
+
+  ngAfterContentInit() {
+    this.getEntry();
+  }
+
+  getEntry() {
+    let user = JSON.parse(localStorage.getItem('user') || '{}');
+    var params = {
+      session:user.id,
+      module_name:"Accounts",
+      id:this.id,
+      select_fields:[],
+    }
+    console.log(params)
+    this.apiService.CALL(params, "get_entry").then((jsonData:any) => {
+      // var data = JSON.parse(jsonData)
+      console.log(jsonData)
+    }).catch((error) => {
+      console.warn('Error fetching JSON data:', error);
+    });
   }
 
   makeEditable() {
@@ -30,9 +53,13 @@ export class AccountsDetailViewComponent implements OnInit {
   }
   
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
     this.myForm = this.formBuilder.group({
 
     });
+
   }
 
   saveForm() {
