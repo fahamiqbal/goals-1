5a4e03c5ae0a3514f61103991b14ae482e6e5efb
Faham Iqbal <faham.iqbal@rolustech.com>
2023-06-28 17:12:15 +0500

    Removing all extra hrefs

--- src/app/layouts/auth/register/register.component.ts
+++ src/app/layouts/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {ApiService} from "../../../core/crm-api/api.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  registerForm = this.formBuilder.group({
+    username: '',
+    password: '',
+    password_repeat: ''
+  });
+
+  constructor(private formBuilder: FormBuilder,private service:ApiService,private router: Router) {}
+
+  async onSubmit() {
+    var params = {
+        $username: this.registerForm.controls['username'].value,
+        password: this.registerForm.controls['password'].value,
+    };
+    var response = await this.service.customCALL(params, 'createUser');
+    console.log(response)
+    // var res = JSON.parse(response);
+    // if(res.id)
+    // {
+    //   localStorage.setItem('user',JSON.stringify(res));
+    //   this.router.navigate(['/dashboard/Home']);
+    // }
+    // else
+    // {
+    //   alert(res.description);
+    // }
+    console.warn('Your order has been submitted', this.registerForm.value);
+    this.registerForm.reset();
+  }
 
 }
